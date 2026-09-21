ec27bf0e42fd4d1eefe3d5368d62b395a5c331b2
soto-cmd <69586667+soto-cmd@users.noreply.github.com>
2026-09-21 08:55:47 -0300

    Refresh Cecilia v2 service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='cecilia-v9';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./privacy.js','./crud.js','./cloudfix.js','./manifest.webmanifest'];
+const CACHE='cecilia-v10';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
