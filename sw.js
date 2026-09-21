e42ad4cfa7d08a4ebd4a44ac1072cef7f68620a9
soto-cmd <69586667+soto-cmd@users.noreply.github.com>
2026-09-21 08:22:03 -0300

    Actualizar caché para acciones de edición y eliminación

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='cecilia-v5';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='cecilia-v6';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./crud.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
