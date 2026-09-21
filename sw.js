2a13413a488cba987f6b5ea425b4cfdf81aec1f1
soto-cmd <69586667+soto-cmd@users.noreply.github.com>
2026-09-21 07:07:14 -0300

    Actualizar cache para respaldo en nube

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cecilia-v3';
+const CACHE='cecilia-v4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
