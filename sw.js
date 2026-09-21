a797c44567e08c3eb06a26d6f11d186047d08401
soto-cmd <69586667+soto-cmd@users.noreply.github.com>
2026-09-21 08:35:21 -0300

    Refresh Cecilia cache after sync optimization

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cecilia-v7';
+const CACHE='cecilia-v8';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./privacy.js','./crud.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
